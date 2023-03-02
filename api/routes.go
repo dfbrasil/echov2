f25134472ad5915e256cea7a6199cd80688332c9
Daniel Brasil <danieldfb@gmail.com>
2023-03-02 19:45:13 -0300

    crud funcionando

---
+++ api/routes.go
@@ -0,0 +1,21 @@
+package api
+
+import (
+
+	"github.com/labstack/echo/v4"
+)
+
+func (a *API) RegisterRoutes(e *echo.Echo) { //registra as rotas da API
+	//(a *API é um receiver)
+	//REgisterRoutes recebe um router de ECHO
+
+	e.Use(requestIDHandler)
+
+	public := e.Group("")
+	protected := e.Group("", authMidlleware)
+
+	public.GET("/books", a.getBooks)
+	public.GET("/books/:id", a.getBook)
+
+	protected.POST("/books", a.postBook)
+}
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"echov2/api"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func handleIndex(c echo.Context) error{ //as funções em Echo devem retornar um error
+	return c.JSON(http.StatusOK, map[string]string{"message":"hello motherfuckers"})//echo possui uma função no context que se chama JSON, que recebe um status e um dado(interface)
+}
+
+func main(){
+	e := echo.New() // um objeto de ECHO que está retornando um ponteiro de ECHO
+
+	e.GET("/", handleIndex) //a este objeto Echo está mapedao uma rota
+
+	a := &api.API{}
+	a.RegisterRoutes(e)
+
+	e.Logger.Fatal(e.Start(":8080"))//se ocorre algum error na inicialização será mostrado no terminal
+}
